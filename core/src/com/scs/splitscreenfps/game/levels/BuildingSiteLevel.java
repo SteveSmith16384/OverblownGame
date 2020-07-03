03db6c7d03121cb24dea8b7edd669738ad88ee83
Stephen Carlyle-Smith <StephenCS@ecmsys.local>
2020-07-03 11:40:11 +0100

    Got uprights in building site to show

--- core/src/com/scs/splitscreenfps/game/levels/BuildingSiteLevel.java
+++ core/src/com/scs/splitscreenfps/game/levels/BuildingSiteLevel.java
@@ -47,34 +47,39 @@ public class BuildingSiteLevel extends AbstractLevel {
 		String csv = file.readString();
 		String rows[] = csv.split("\n");
 		for (int row=0 ; row<rows.length ; row++) {
-			decodeRow(rows[row].toLowerCase(), rows, row);
+			boolean end = decodeRow(rows[row].toLowerCase(), rows, row);
+			if (end) {
+				break;
+			}
 		}
 	}
 
 
-	private void decodeRow(String data, String rows[], int current_row) {
+	private boolean decodeRow(String data, String rows[], int current_row) {
 		if (data.startsWith("\t")) { // Blank row
 			// DO nothing
 		} else if (data.startsWith("texture")) {
-			String[] tokens = data.split(":");
+			String[] tokens = data.trim().split(":");
 			this.textures.put(tokens[1], tokens[2]);
 		} else if (data.startsWith("map")) {
-			this.decodeMap(rows, current_row);
+			this.decodeMap(rows, current_row+1);
+			return true; // Map is last
 		} else {
-			throw new RuntimeException("Todo");
+			throw new RuntimeException("Unknown data: " + data);
 		}
+		return false;
 	}
 
 
 	private void decodeMap(String rows[], int start_row) {
-		for (int row=start_row+1 ; row<rows.length ; row++) {
+		for (int row=start_row ; row<rows.length ; row++) {
 			String cols[] = rows[row].split("\t");
 			for (int col=0 ; col<cols.length ; col++) {
 				//String tokens[] = cols[col].split("~");
 				//for (int t=0 ; t<tokens.length ; t++) {
-					if (cols[col].trim().length() > 0) {
-						decodeMapItem(cols[col].trim(), col, row);
-					}
+				if (cols[col].trim().length() > 0) {
+					decodeMapItem(cols[col].trim(), col, row-start_row);
+				}
 				//}
 			}
 		}		
@@ -82,27 +87,40 @@ public class BuildingSiteLevel extends AbstractLevel {
 
 
 	private void decodeMapItem(String data, int col, int row) {
-		String items[] = data.split("~");
-		for (int i=0 ; i<items.length ; i++) {
-			if (items[i].equalsIgnoreCase("Cube")) {
+		String items[] = data.trim().split("~");
+		//for (int i=0 ; i<items.length ; i++) {
+			if (items[0].equalsIgnoreCase("Cube")) {
 				parseCube(items, col, row);
 			} else {
-				throw new RuntimeException("Unknown code: " + items[i]);
+				throw new RuntimeException("Unknown code: " + items[0]);
 			}
-		}
+		//}
 	}
 
 
 	private void parseCube(String[] items, int col, int row) {
+		boolean got_pos = false;
 		Vector3 pos = new Vector3(col, 0, row);
 		Vector3 size = new Vector3();
 		String tex = null;
 		float mass = 0;
+		String name = "Todo";
 		
 		//String items[] = data.split("~");
 		for (int i=1 ; i<items.length ; i++) {
 			String[] split = items[i].split(":");
-			if (split[0].equalsIgnoreCase("w")) {
+			if (split[0].equalsIgnoreCase("name")) {
+				name = split[1];
+			} else if (split[0].equalsIgnoreCase("x")) {
+				pos.x = Float.parseFloat(split[1]);
+				got_pos = true;
+			} else if (split[0].equalsIgnoreCase("y")) {
+				pos.y = Float.parseFloat(split[1]);
+				got_pos = true;
+			} else if (split[0].equalsIgnoreCase("z")) {
+				pos.z = Float.parseFloat(split[1]);
+				got_pos = true;
+			} else if (split[0].equalsIgnoreCase("w")) {
 				size.x = Float.parseFloat(split[1]);
 			} else if (split[0].equalsIgnoreCase("h")) {
 				size.y = Float.parseFloat(split[1]);
@@ -116,8 +134,16 @@ public class BuildingSiteLevel extends AbstractLevel {
 				throw new RuntimeException("Unknown code: " + items[i]);
 			}
 		}
+		
+		if (got_pos == false) {
+			//pos.set(size).scl(.5f);
+			pos.y = size.y/2;
+		}
+		if (tex == null) {
+			tex = "colours/white.png";
+		}
 
-		AbstractEntity entity = new Wall(game.ecs, "Name", tex, pos.x, pos.y, pos.z, size.x, size.y, size.z, mass, 0, 0, 0, false, true);
+		AbstractEntity entity = new Wall(game.ecs, name, tex, pos.x, pos.y, pos.z, size.x, size.y, size.z, mass, 0, 0, 0, false, true);
 		game.ecs.addEntity(entity);
 	}
 
